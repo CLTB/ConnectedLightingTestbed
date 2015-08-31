0aaa85c8f367d697bce3ac1a8633b24a10af164f
lucyahuang <d3x632@we27539.pnl.gov>
2015-08-31 13:07:47 -0700

    create open connection

--- script/phptest.php
+++ script/phptest.php
@@ -1,6 +1,20 @@
 <?php
 	phpinfo();
-	$connectionInfo = array("UID" => "ConnLghtTb@xs7joiu9pz", "pwd" => "SSLighting!", "Database" => "records", "LoginTimeout" => 30, "Encrypt" => 1);
-	$serverName = "tcp:xs7joiu9pz.database.windows.net,1433";
-	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	$conn = OpenConnection();
+	
+	function OpenConnection()
+	{
+		try
+		{
+			$connectionInfo = array("UID" => "ConnLghtTb@xs7joiu9pz", "pwd" => "SSLighting!", "Database" => "records", "LoginTimeout" => 30, "Encrypt" => 1);
+			$serverName = "tcp:xs7joiu9pz.database.windows.net,1433";
+			$conn = sqlsrv_connect($serverName, $connectionInfo);
+			if($conn == false)
+				die(FormatErrors(sqlsrv_errors()));
+		}
+		catch(Exception $e)
+		{
+			echo("Error!");
+		}
+	}
 ?>
