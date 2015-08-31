d9cea6f39902efded88c5c9cf6c5c7a842a09e64
lucyahuang <d3x632@we27539.pnl.gov>
2015-08-31 13:03:18 -0700

    add server connection

--- script/phptest.php
+++ script/phptest.php
@@ -1,3 +1,6 @@
 <?php
 	phpinfo();
+	$connectionInfo = array("UID" => "ConnLghtTb@xs7joiu9pz", "pwd" => "SSLighting!", "Database" => "records", "LoginTimeout" => 30, "Encrypt" => 1);
+	$serverName = "tcp:xs7joiu9pz.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
